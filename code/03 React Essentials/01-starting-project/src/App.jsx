5b1136b1132730af4d3c6075e4f20ecadfe82d98
yauhen-sobaleu <yauhen.sobaleu@gmail.com>
2025-04-13 23:30:58 +0300

    01-react

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,11 +1,26 @@
-import { CORE_CONCEPTS } from './data.js';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx';
 import TabButton from './components/TabButton/TabButton.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Dynamic Content');
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  let tabContent = <p>Please select a topic</p>
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>
+            {EXAMPLES[selectedTopic].code}
+          </code>
+        </pre>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -22,11 +37,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-              <TabButton onClick={() => setSelectedTopic('Dynamic Content 1')}>Example 1</TabButton>
-              <TabButton onClick={() => setSelectedTopic('Dynamic Content 2')}>Example 2</TabButton>
-              <TabButton onClick={() => setSelectedTopic('Dynamic Content 3')}>Example 3</TabButton>
+              <TabButton onClick={() => setSelectedTopic('components')}>Components</TabButton>
+              <TabButton onClick={() => setSelectedTopic('jsx')}>JSX</TabButton>
+              <TabButton onClick={() => setSelectedTopic('props')}>Props</TabButton>
+              <TabButton onClick={() => setSelectedTopic('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {tabContent}
         </section>
       </main>
     </>
