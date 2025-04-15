8898223d64674005f78bfdbba359d7520fe1c960
yauhen-sobaleu <yauhen.sobaleu@gmail.com>
2025-04-15 18:13:36 +0300

    add state to player

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player/Player.jsx
@@ -1,13 +1,31 @@
 import "./Player.css";
+import { useState } from "react";
 
 function Player({ icon, name}) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEdit() {
+    if (isEditing) {
+      setIsEditing(false);
+    } else {
+      setIsEditing(true);
+    }
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
+
+  const buttonText = isEditing ? "Save" : "Edit";
+
   return (
     <li>
         <span className="player">
-            <span className="player-icon">{icon}</span>
-            <span className="player-name">{name}</span>
+          {playerName}
+          <span className="player-icon">{icon}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={handleEdit}>{buttonText}</button>
     </li>
   )
 }
