74206854bb1281a0097f3fab67dace51877fa966
yauhen-sobaleu <yauhen.sobaleu@gmail.com>
2025-05-12 19:30:50 +0300

    add button

--- code/09 Practice Project - Project Management/01-starting-project/src/components/Button.tsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/components/Button.tsx
@@ -1,10 +1,11 @@
 import { buttonStyles } from '../styles/input-styles';
 
-export default function Button({ children, variant, onClick = undefined }) {
+export default function Button({ children, form, variant, onClick = undefined }) {
   const buttonStyle = buttonStyles[variant];
 
   return (
     <button
+      form={form}
       className={`
             px-4 
             py-2 
--- code/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.tsx
+++ code/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.tsx
@@ -7,20 +7,27 @@ export default function NewProject() {
   const inputMargin = 'mb-4';
 
   return (
-    <div className="px-8 w-10/12">
-      <Button variant={ButtonVariant.BLACK}>Save</Button>
+    <>
+      <div className="px-8 w-10/12">
+        <div className="text-right">
+          <Button variant={ButtonVariant.WHITE}>Cancel</Button>
+          <Button form="new-project-form" variant={ButtonVariant.BLACK}>
+            Save
+          </Button>
+        </div>
 
-      <form id="new-project-form">
-        <div className={inputMargin}>
-          <Input id="title" type="text" label="Title" />
-        </div>
-        <div className={inputMargin}>
-          <TextArea id="description" label="Description" />
-        </div>
-        <div className={inputMargin}>
-          <Input id="duedate" type="date" label="Due Date" />
-        </div>
-      </form>
-    </div>
+        <form id="new-project-form">
+          <div className={inputMargin}>
+            <Input id="title" type="text" label="Title" />
+          </div>
+          <div className={inputMargin}>
+            <TextArea id="description" label="Description" />
+          </div>
+          <div className={inputMargin}>
+            <Input id="duedate" type="date" label="Due Date" />
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
